64f14cba339a5d6dcee9fbfc75bfc389f809bfbe
veso266 <veso266@gmail.com>
2026-01-12 11:59:43 +0100

    Restrict allowedHosts to specific domain

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,6 +6,6 @@ export default defineConfig({
     plugins: [react()],
     server: {
         port: 3000,
-        allowedHosts: "all",
+        allowedHosts: ["radioservice.dxing.si"],
     },
 });
